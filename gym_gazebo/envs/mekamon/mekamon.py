3066586afd8276fc0874d87bc222c66057b0fc1c
brothergomez <38558969+brothergomez@users.noreply.github.com>
2019-02-07 22:43:43 +0000

    Create mekamon.py

---
+++ gym_gazebo/envs/mekamon/mekamon.py
@@ -0,0 +1,67 @@
+import gym
+import rospy
+import roslaunch
+import time
+import numpy as np
+
+from gym import utils, spaces
+from gym_gazebo.envs import gazebo_env
+from std_srvs.srv import Empty
+
+from gym.utils import seeding
+
+class GazeboMekamonEnv(gazebo_env.GazeboEnv):
+
+    def __init__(self):
+        pass
+        
+    def step(self, action):
+
+         Parameters
+        ----------
+        action :
+
+        Returns
+        -------
+        ob, reward, episode_over, info : tuple
+            ob (object) :
+                an environment-specific object representing your observation of
+                the environment.
+            reward (float) :
+                amount of reward achieved by the previous action. The scale
+                varies between environments, but the goal is always to increase
+                your total reward.
+            episode_over (bool) :
+                whether it's time to reset the environment again. Most (but not
+                all) tasks are divided up into well-defined episodes, and done
+                being True indicates the episode has terminated. (For example,
+                perhaps the pole tipped too far, or you lost your last life.)
+            info (dict) :
+                 diagnostic information useful for debugging. It can sometimes
+                 be useful for learning (for example, it might contain the raw
+                 probabilities behind the environment's last state change).
+                 However, official evaluations of your agent are not allowed to
+                 use this for learning.
+        """
+        self._take_action(action)
+        self.status = self.env.step()
+        reward = self._get_reward()
+        ob = self.env.getState()
+        episode_over = self.status != hfo_py.IN_GAME
+        return ob, reward, episode_over, {}
+    def reset(self):
+        pass
+       def _render(self, mode='human', close=False):
+        pass
+
+    def _take_action(self, action):
+        pass
+
+    def _get_reward(self):
+        """ Reward is given for XY. """
+        if self.status == 1:
+            return 1
+        elif self.status == 2:
+            return self.somestate ** 2
+        else:
+            return 0
