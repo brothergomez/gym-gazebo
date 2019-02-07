611bfd23128100e20dade4aaf2773ae638640371
brothergomez <38558969+brothergomez@users.noreply.github.com>
2019-02-07 22:37:48 +0000

    Create mekamon_setup.bash

---
+++ gym_gazebo/envs/installation/mekamon_setup.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$GAZEBO_MODEL_PATH" ]; then
+  bash -c 'echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:"`pwd`/../assets/models >> ~/.bashrc'
+else
+  bash -c 'sed "s,GAZEBO_MODEL_PATH=[^;]*,'GAZEBO_MODEL_PATH=`pwd`/../assets/models'," -i ~/.bashrc'
+fi
+
+
+
+#copy altered urdf model
+cp -r ../assets/urdf/mekamon/* catkin_ws/src/mekamon_urdf/mekamon_urdf/urdf/
