fbc59da4ac98f5258733ea87dc88193e5f3a70c9
Matt Haddock <matt@reachrobotics.com>
2019-02-08 16:16:42 +0000

    trying random actions

--- examples/mekamon/mekamon_example_v0.py
+++ examples/mekamon/mekamon_example_v0.py
@@ -9,4 +9,7 @@ import random
 for i in range(100):
     env.reset()
     print("Reset!")
-    time.sleep(60)
+    for x in range(200):
+        observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
+        # if done: break
+
